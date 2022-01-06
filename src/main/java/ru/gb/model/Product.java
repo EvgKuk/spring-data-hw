3d6e8370cf995601bd27277d627cb25b222f8070
Evgeniy Kukushkin <eugin86@list.ru>
2022-01-06 16:01:56 +0500

    ДЗ для урока 7 Не понимаю почему метод add не работает через rest ... Как его реализовать? после add/что-то нужно прописать что добавить, какой синтаксис должен быть?

---
+++ src/main/java/ru/gb/model/Product.java
@@ -0,0 +1,23 @@
+package ru.gb.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name="product")
+@Data
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+    @Column
+    private String name;
+    @Column
+    private Float cost;
+}
